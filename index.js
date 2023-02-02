3244ae851de03e2759febcc8acd01489bfd9e8ec
thankgodo <thankgodobiekwe@gmail.com>
2023-02-02 15:10:47 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+
+const nam1 = prompt("Enter your first name:");
+const nam2 = prompt("Enter your last name:");
+const nam3 = nam1 + nam2
+alert(`Your full name is; ${nam1}  ${nam2}, and your username is; ${nam3}`);
+
+const nam1s = nam1.slice(0,1);
+const nam2s = nam2.slice(0,4)
+const unam = nam1s + nam2s
+
+const lower = unam.toLocaleLowerCase();
+
+
+document.getElementById("challenge").innerHTML = (lower);
